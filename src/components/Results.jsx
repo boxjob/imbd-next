0f529df5f274ee760d0d404a4722f8af790dc719
boxjob <boxjob@gmail.com>
2024-03-12 20:32:55 -0300

    Update Result component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,7 +2,7 @@ const { default: Card } = require("./Card")
 
 const Results = ({ results }) => {
     return (
-        <div className="grid sm:grid-col-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto gap-8 m-4 p-4 ">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto gap-8 m-4 p-4 ">
             { results.map( e => 
                 <Card key={ e.id } result={ e } />
             )}
